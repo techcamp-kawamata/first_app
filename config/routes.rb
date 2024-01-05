2202fbea26f54fc291c25349970a4845cc8ad7ef
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-01-05 15:56:51 +0900

    posts#indexへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
